18b59d04d88b226f46db9edc2a7ff51b0a1075ec
Squagilli <cotrupeconnor@gmail.com>
2021-07-11 15:23:05 +0200

    testing DOM manipulation

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+
+const gridA = document.createElement('div');
+gridA.classList.add('gridA');
+gridA.textContent = 'hello';
+
+container.appendChild(gridA);
