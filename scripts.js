f6d0bf17e5702d452eb9d9e96c119c8c7db08ce6
Squagilli <cotrupeconnor@gmail.com>
2021-07-11 17:52:48 +0200

    using different method to create grid

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 const container = document.querySelector('#container');
 
-const grid1 = document.createElement('div');
-grid1.classList.add('grid1');
-grid1.textContent = 'hello';
+let grid = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 
+'g10', 'g11', 'g12', 'g13', 'g14', 'g15', 'g16', 'g17', 'g18',
+ 'g19', 'g20', 'g21', 'g22', 'g23', 'g24', 'g25', 'g26', 'g27',
+  'g28', 'g29', 'g30', 'g31', 'g32'];
 
-container.appendChild(grid1);
+
+
+grid.textContent = 'bruh';
+
+for (i = 0; i < grid.length; i++) {
+    container.append(grid);
+}
