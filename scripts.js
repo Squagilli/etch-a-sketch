adf48a23fafa45a51e7283ae9bd9510408e8a8cf
Squagilli <cotrupeconnor@gmail.com>
2021-07-12 17:07:41 +0200

    figuring out how to make a grid in js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
 const container = document.querySelector('#container');
 
-let grid = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 
-'g10', 'g11', 'g12', 'g13', 'g14', 'g15', 'g16', 'g17', 'g18',
- 'g19', 'g20', 'g21', 'g22', 'g23', 'g24', 'g25', 'g26', 'g27',
-  'g28', 'g29', 'g30', 'g31', 'g32'];
+function makeGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+        for (i = 0; i < (rows * cols); i++) {
+            let cell = document.createElement('div');
+            cell.innerText = (i + 1);
+            container.appendChild(cell).className = 'gridItem';
+        };
+};
 
-
-
-grid.textContent = 'bruh';
-
-for (i = 0; i < grid.length; i++) {
-    container.append(grid);
-}
+makeGrid(16, 16);
