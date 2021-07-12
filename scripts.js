c1871c6825f10ffe61a4f16be3b4b1b2ea7c1084
Squagilli <cotrupeconnor@gmail.com>
2021-07-12 22:26:41 +0200

    Some CSS

--- scripts.js
+++ scripts.js
@@ -2,6 +2,15 @@ const container = document.querySelector('#container');
 
 const gridItem = document.querySelector('.cell');
 
+const clearBtn = document.querySelector('.clearBtn');
+clearBtn.innerText = 'Clear';
+
+const optBtn = document.querySelector('.options');
+optBtn.innerText = 'Options';
+
+const header = document.querySelector('#header');
+header.innerText = 'Etch-a-Sketch';
+
 
 function makeCells() {
     for (i = 0; i < 4096; i++){
@@ -10,9 +19,8 @@ function makeCells() {
         container.appendChild(cell);
     }
 }
+
 makeCells();
-
-
 
 function drawPicture(e) {
     
@@ -23,6 +31,10 @@ function drawPicture(e) {
 const cells = document.querySelectorAll('.cell');
 
 cells.forEach(cell => cell.addEventListener('mouseover', drawPicture));
+
+
+
+
 
 
 
