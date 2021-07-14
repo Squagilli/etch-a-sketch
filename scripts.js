186706828954c6968b43caba6410477efd9557d1
Squagilli <cotrupeconnor@gmail.com>
2021-07-14 16:29:38 +0200

    added colors, added slider to change grid size, added media query for mobile functionality

--- scripts.js
+++ scripts.js
@@ -11,15 +11,71 @@ optBtn.innerText = 'Colors';
 const header = document.querySelector('#header');
 header.innerText = 'Etch-a-Sketch';
 
+const slider = document.querySelector('#sizeRange');
 
 
-function makeCells(rows, columns) { // generates cells of specified size
-    for (i = 0; i < (rows * columns); i++){
-        const cell = document.createElement('div');
+
+function makeCells(resolution) { // generates cells of specified size
+    let cellArea = resolution * resolution;
+    for (i = 0; i <= cellArea; i++){
+        let cell = document.createElement('div');
         cell.classList.add('cell');
-        container.appendChild(cell);
-        
+        //container.appendChild(cell);
+        container.style.gridTemplateRows = `repeat(${resolution}, 1fr )`;
+        container.style.gridTemplateColumns = `repeat(${resolution}, 1fr)`;
+        container.insertAdjacentElement('beforeend', cell);
     }
+    const cells = [...document.querySelectorAll('.cell')];
+
+    const blueColor = document.querySelector('.blue')
+    const redColor = document.querySelector('.red');
+    const greenColor = document.querySelector('.green');
+    const yellowColor = document.querySelector('.yellow');
+    const orangeColor = document.querySelector('.orange');
+    const violetColor = document.querySelector('.violet');
+    const indigoColor = document.querySelector('.indigo');
+    const blackColor = document.querySelector('.black');
+    const whiteColor = document.querySelector('.white');
+
+    cells.forEach(cell => cell.onmouseover = drawPictureBlue);
+
+    blueColor.addEventListener('click', function() {
+        cells.forEach(cell => cell.onmouseover = drawPictureBlue);
+    })
+    redColor.addEventListener('click', function() {
+        cells.forEach(cell => cell.onmouseover = drawPictureRed);
+    })
+    greenColor.addEventListener('click', function() {
+        cells.forEach(cell => cell.onmouseover = drawPictureGreen);
+    })
+    yellowColor.addEventListener('click', function() {
+        cells.forEach(cell => cell.onmouseover = drawPictureYellow);
+    })
+    orangeColor.addEventListener('click', function() {
+        cells.forEach(cell => cell.onmouseover = drawPictureOrange);
+    })
+    violetColor.addEventListener('click', function() {
+        cells.forEach(cell => cell.onmouseover = drawPictureViolet);
+    })
+    indigoColor.addEventListener('click', function() {
+        cells.forEach(cell => cell.onmouseover = drawPictureIndigo);
+    })
+    blackColor.addEventListener('click', function() {
+        cells.forEach(cell => cell.onmouseover = drawPictureBlack);
+    })
+    whiteColor.addEventListener('click', function() {
+        cells.forEach(cell => cell.onmouseover = drawPictureWhite);
+    })
+
+    clearBtn.addEventListener('click', function () {
+        document.querySelectorAll('.cell').forEach(item => item.style.backgroundColor = '#182628');
+    })
+}
+
+function changeSize(){
+    let gridCells = container.querySelectorAll('div');
+    gridCells.forEach(gridCell => gridCell.remove());
+    makeCells(slider.value);
 }
 
 
@@ -27,16 +83,11 @@ const small = document.querySelector('.resolutionSmall');
 const medium = document.querySelector('.resolutionMed');
 const large = document.querySelector('resolutionLarge');
 
-let length = 64;
-let width = 64;
+
+makeCells(16);
 
 
-
-
-
-
-
-makeCells(length, width);
+//makeCells(resolution);
 
 
 function drawPictureBlue(e) {
@@ -60,8 +111,14 @@ function drawPictureIndigo(e) {
 function drawPictureViolet(e) {
     e.target.closest('.cell').style.backgroundColor = 'violet';
 }
+function drawPictureBlack(e) {
+    e.target.closest('.cell').style.backgroundColor = 'black';
+}
+function drawPictureWhite(e) {
+    e.target.closest('.cell').style.backgroundColor = 'white';
+}
 
-const cells = [...document.querySelectorAll('.cell')];
+/*const cells = [...document.querySelectorAll('.cell')];
 
 const blueColor = document.querySelector('.blue')
 const redColor = document.querySelector('.red');
@@ -70,11 +127,12 @@ const yellowColor = document.querySelector('.yellow');
 const orangeColor = document.querySelector('.orange');
 const violetColor = document.querySelector('.violet');
 const indigoColor = document.querySelector('.indigo');
+const blackColor = document.querySelector('.black');
+const whiteColor = document.querySelector('.white');
 
 cells.forEach(cell => cell.onmouseover = drawPictureBlue);
 
 blueColor.addEventListener('click', function() {
-    
     cells.forEach(cell => cell.onmouseover = drawPictureBlue);
 })
 redColor.addEventListener('click', function() {
@@ -95,11 +153,17 @@ violetColor.addEventListener('click', function() {
 indigoColor.addEventListener('click', function() {
     cells.forEach(cell => cell.onmouseover = drawPictureIndigo);
 })
+blackColor.addEventListener('click', function() {
+    cells.forEach(cell => cell.onmouseover = drawPictureBlack);
+})
+whiteColor.addEventListener('click', function() {
+    cells.forEach(cell => cell.onmouseover = drawPictureWhite);
+})
 
 clearBtn.addEventListener('click', function () {
     document.querySelectorAll('.cell').forEach(item => item.style.backgroundColor = '#182628');
-})
-
+})*/
+slider.addEventListener('mouseup', changeSize);
 
 
 
