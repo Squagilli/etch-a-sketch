59e9acc0ec8b9a5cbe390f56f342fd833f7e120b
Squagilli <cotrupeconnor@gmail.com>
2021-07-14 16:38:05 +0200

    cleaned up js code

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+
+// various objects in document
 const container = document.querySelector('#container');
 
 const gridItem = document.querySelector('.cell');
@@ -20,13 +22,13 @@ function makeCells(resolution) { // generates cells of specified size
     for (i = 0; i <= cellArea; i++){
         let cell = document.createElement('div');
         cell.classList.add('cell');
-        //container.appendChild(cell);
         container.style.gridTemplateRows = `repeat(${resolution}, 1fr )`;
         container.style.gridTemplateColumns = `repeat(${resolution}, 1fr)`;
         container.insertAdjacentElement('beforeend', cell);
     }
-    const cells = [...document.querySelectorAll('.cell')];
+    const cells = [...document.querySelectorAll('.cell')]; // all cells selector
 
+    // color buttons
     const blueColor = document.querySelector('.blue')
     const redColor = document.querySelector('.red');
     const greenColor = document.querySelector('.green');
@@ -37,7 +39,8 @@ function makeCells(resolution) { // generates cells of specified size
     const blackColor = document.querySelector('.black');
     const whiteColor = document.querySelector('.white');
 
-    cells.forEach(cell => cell.onmouseover = drawPictureBlue);
+    // event listeners for mouseover on cells
+    cells.forEach(cell => cell.onmouseover = drawPictureBlue); // default color
 
     blueColor.addEventListener('click', function() {
         cells.forEach(cell => cell.onmouseover = drawPictureBlue);
@@ -69,25 +72,17 @@ function makeCells(resolution) { // generates cells of specified size
 
     clearBtn.addEventListener('click', function () {
         document.querySelectorAll('.cell').forEach(item => item.style.backgroundColor = '#182628');
-    })
+    }) // clears all color
 }
 
-function changeSize(){
+function changeSize(){ 
     let gridCells = container.querySelectorAll('div');
     gridCells.forEach(gridCell => gridCell.remove());
     makeCells(slider.value);
-}
+} // changes size of cells based on slider value
 
 
-const small = document.querySelector('.resolutionSmall');
-const medium = document.querySelector('.resolutionMed');
-const large = document.querySelector('resolutionLarge');
-
-
-makeCells(16);
-
-
-//makeCells(resolution);
+makeCells(16); // default set for mobile compatability
 
 
 function drawPictureBlue(e) {
@@ -118,51 +113,6 @@ function drawPictureWhite(e) {
     e.target.closest('.cell').style.backgroundColor = 'white';
 }
 
-/*const cells = [...document.querySelectorAll('.cell')];
-
-const blueColor = document.querySelector('.blue')
-const redColor = document.querySelector('.red');
-const greenColor = document.querySelector('.green');
-const yellowColor = document.querySelector('.yellow');
-const orangeColor = document.querySelector('.orange');
-const violetColor = document.querySelector('.violet');
-const indigoColor = document.querySelector('.indigo');
-const blackColor = document.querySelector('.black');
-const whiteColor = document.querySelector('.white');
-
-cells.forEach(cell => cell.onmouseover = drawPictureBlue);
-
-blueColor.addEventListener('click', function() {
-    cells.forEach(cell => cell.onmouseover = drawPictureBlue);
-})
-redColor.addEventListener('click', function() {
-    cells.forEach(cell => cell.onmouseover = drawPictureRed);
-})
-greenColor.addEventListener('click', function() {
-    cells.forEach(cell => cell.onmouseover = drawPictureGreen);
-})
-yellowColor.addEventListener('click', function() {
-    cells.forEach(cell => cell.onmouseover = drawPictureYellow);
-})
-orangeColor.addEventListener('click', function() {
-    cells.forEach(cell => cell.onmouseover = drawPictureOrange);
-})
-violetColor.addEventListener('click', function() {
-    cells.forEach(cell => cell.onmouseover = drawPictureViolet);
-})
-indigoColor.addEventListener('click', function() {
-    cells.forEach(cell => cell.onmouseover = drawPictureIndigo);
-})
-blackColor.addEventListener('click', function() {
-    cells.forEach(cell => cell.onmouseover = drawPictureBlack);
-})
-whiteColor.addEventListener('click', function() {
-    cells.forEach(cell => cell.onmouseover = drawPictureWhite);
-})
-
-clearBtn.addEventListener('click', function () {
-    document.querySelectorAll('.cell').forEach(item => item.style.backgroundColor = '#182628');
-})*/
 slider.addEventListener('mouseup', changeSize);
 
 
